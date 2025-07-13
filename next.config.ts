8114da61e5b3f8af5b018240e8f830e6d050c48d
xxxijustwei <owner@ednesdayw.com>
2025-07-13 14:39:35 +0800

    refactor: simplify path import and config formatting

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,21 @@
-import type { NextConfig } from "next";
+import path from "node:path";
 import { withContentCollections } from "@content-collections/next";
-import path from "path";
+import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const config: NextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                hostname: "ipfs.io",
-            },
-        ],
-    },
-    webpack: (config, _context) => {
-        config.resolve.alias["jotai"] = path.resolve(
-            __dirname,
-            "node_modules/jotai",
-        );
-        return config;
-    },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "ipfs.io",
+      },
+    ],
+  },
+  webpack: (config, _context) => {
+    config.resolve.alias.jotai = path.resolve(__dirname, "node_modules/jotai");
+    return config;
+  },
 };
 
 export default withContentCollections(config);
