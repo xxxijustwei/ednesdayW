2aa9d62e74583af2f02d9cbee566ba45ee25a6d3
xxxijustwei <owner@ednesdayw.com>
2025-07-20 22:48:01 +0800

    feat: update wallet address test cases

--- src/registry/lib/wallet-address.test.ts
+++ src/registry/lib/wallet-address.test.ts
@@ -2,15 +2,15 @@ import { describe, expect, it, test } from "vitest";
 import { isAddress } from "./wallet-address";
 
 test("check wallet address is valid", () => {
-  expect(isAddress("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045")).toBe(true);
-  expect(isAddress("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA98888")).toBe(false);
+  expect(isAddress("0x888777d851872Cd075D88C61BE8793Ff302C1FAd")).toBe(true);
+  expect(isAddress("0x888999d851872Cd075D88C61BE8793Ff302C1FAd")).toBe(false);
   expect(isAddress("0xabc")).toBe(false);
-  expect(isAddress("TU3kjFuhtEo42tsCBtfYUAZxoqQ4yuSLQ5", "tron")).toBe(true);
-  expect(isAddress("TU3kjFuhtEo42tsCBtfYUAZxoqQ4yuSLQ8", "tron")).toBe(false);
+  expect(isAddress("TWJ88TmUTG3ZcvPqR8WrPxA33esoXFFFFF", "tron")).toBe(true);
+  expect(isAddress("TWJ88TmUTG3ZcvPqR8WrPxA33esoX77777", "tron")).toBe(false);
   expect(
-    isAddress("52C9T2T7JRojtxumYnYZhyUmrN7kqzvCLc4Ksvjk7TxD", "solana"),
+    isAddress("FQh8Vkasb6h173nJ7mXmHwmbV7DX81N6jXBaNQNaP5RA", "solana"),
   ).toBe(true);
-  expect(isAddress("AJKqp326RZCHnAAbew9MDdui3iCKWco7f9sVuZTX2", "solana")).toBe(
-    false,
-  );
+  expect(
+    isAddress("FQh8Vkasb6h173nJ7mXmHwmbV7DX81N6jXBaNNNNNNN", "solana"),
+  ).toBe(false);
 });
